c1796291139e39a6cdf8bfb94a382b1c72849027
Shuran Ubuntu <zhoushuran0731@gmail.com>
2017-05-22 00:13:02 +0800

    some update

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ def feature_normalize(dataset):
     sigma = np.std(dataset, axis=0)
     return (dataset - mu) / sigma
 
-
-def
-
-
-
+def windows(data, size):
+    start = 0
+    while start < data.count():
+        yield start, start + size
+        start += (size / 2)
 
 def segment_signal(data, window_size=90):
     segments = np.empty((0, window_size, 3))
@@ -44,13 +44,38 @@ def segment_signal(data, window_size=90):
             labels = np.append(labels, stats.mode(data["activity"][start:end])[0][0])
     return segments, labels
 
+def plot_axis(ax, x, y, title):
+    ax.plot(x, y)
+    ax.set_title(title)
+    ax.xaxis.set_visible(False)
+    ax.set_ylim([min(y) - np.std(y), max(y) + np.std(y)])
+    ax.set_xlim([min(x), max(x)])
+    ax.grid(True)
+
+
+def plot_activity(activity, data):
+    fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(15, 10), sharex=True)
+    plot_axis(ax0, data['timestamp'], data['x-axis'], 'x-axis')
+    plot_axis(ax1, data['timestamp'], data['y-axis'], 'y-axis')
+    plot_axis(ax2, data['timestamp'], data['z-axis'], 'z-axis')
+    plt.subplots_adjust(hspace=0.2)
+    fig.suptitle(activity)
+    plt.subplots_adjust(top=0.90)
+    plt.show()
+
+
 
 def main():
 
-    raw_data = read_data(file_path)
-    print raw_data["x-axis"][0:5]
-    print raw_data['timestamp'].shape
-    segment_signal(raw_data, window_size=90)
+    #dataset is a phandas data frame
+    dataset = read_data(file_path)
+    print dataset['z-axis']
+    #normalize each axis
+    dataset['x-axis'] = feature_normalize(dataset['x-axis'])
+    dataset['y-axis'] = feature_normalize(dataset['y-axis'])
+
+    dataset['z-axis'] = feature_normalize(dataset['z-axis'])
+    # segment_signal(raw_data, window_size=90)
 
 
 if __name__ == '__main__':
